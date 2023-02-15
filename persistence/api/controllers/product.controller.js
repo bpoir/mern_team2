1631564001de0af504ec8ad0696bd7451aeb4ab7
Liz <liz.alessandri90@gmail.com>
2023-02-15 12:27:46 -0500

    created product APIs

---
+++ persistence/api/controllers/product.controller.js
@@ -0,0 +1,25 @@
+
+
+const Product = require("../models/product.model")
+
+//build our controller that will have our CRUD and other methods for our users
+const productController = {
+
+    //method to get all users using async/await syntax
+    getProducts: async function(req, res){
+    },
+    //method to create a new user
+    createProduct: async function(req, res){
+    },
+    //method to update a user
+    updateProduct: async function(req, res, next){
+    },
+    //method to get all users using async/await syntax
+    getProduct: async function(req, res){
+    },
+    //method to delete a user
+    deleteProduct: async function(req, res){
+    }
+}
+
+module.exports = productController;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,8 +5,23 @@ const router = express.Router();
 
 //import the user controller to handle our user routes
 const productController = require(
-    `../persistence/${process.env.DB_PROTOCOL}/controllers/user.controller`
+    `../persistence/${process.env.DB_PROTOCOL}/controllers/product.controller`
 )
 
 //post route to create a user (user registration)
 router.post("/", productController.createProduct)
+
+//get route to return all users (requires auth)
+router.get("/", productController.getProduct)
+
+//get route to return a specific users (requires auth)
+router.get("/:name", productController.getProduct)
+
+//put route to update a user (requires auth)
+router.put("/:name", productController.updateProduct)
+
+//delete a user
+router.delete("/:name", productController.deleteProduct)
+
+
+module.exports = router;
